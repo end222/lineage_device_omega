76027bb4853d76024e16e7b41d189aff9d6516c3
end222 <pabloorduna98@gmail.com>
2017-06-12 19:15:38 +0200

    Adapt to compile AICP

--- aicp.mk
+++ aicp.mk
@@ -2,14 +2,14 @@
 PRODUCT_RELEASE_NAME := Redmi Pro
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/omega/full_omega.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := omega
-PRODUCT_NAME := lineage_omega
+PRODUCT_NAME := aicp_omega
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Pro
 PRODUCT_MANUFACTURER := Xiaomi
